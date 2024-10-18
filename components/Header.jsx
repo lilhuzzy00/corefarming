2c4e0014fc6c65bc1809e4dd505cd40787444096
Godspower <chimabest70@gmail.com>
2024-10-18 15:13:18 +0100

    first commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
             ))}
         </div>
       </div>
-      <div className='bg'>
+      <div>
         <div className='bgc'>
           Farming Spirit
         </div>
