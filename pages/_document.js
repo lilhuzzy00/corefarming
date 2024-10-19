83556f01babe1955b98deca799f4186fed43c74b
Godspower <chimabest70@gmail.com>
2024-10-19 20:29:10 +0100

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
+          <meta name="google-site-verification" content="7BTSPvjOkvW48t21X6VE-DZWdtmx6cCcz52p00CoX6I" />
+          <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" />
         </Head>
         <body>
           <Main />
